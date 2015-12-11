41f35db25cb530c836ea4237a5d23790ca24f09b
Steffen Arntz <steffen.arntz@ucware.de>
2015-12-11 14:18:24 +0100

    sanity for missing data

--- berichte.py
+++ berichte.py
@@ -4,8 +4,12 @@ import fileinput
 import subprocess
 import shlex
 
+csvfilename = 'data.csv'
+name = 'Gerrit Kaul'
+abteilung = 'IS-E'
+ausbildungsjahr = 2
 
-with open('data.csv', 'rb') as f:
+with open(csvfilename, 'rb') as f:
     reader = csv.reader(f)
     csvlist = list(reader)
 
@@ -14,23 +18,26 @@ print csvlist
 def descriptionlinebuild(lvalues):
     print('Building Desc')
     description = ''
-    hoursum = lvalues[len(lvalues) -1][3]
-    while(len(lvalues) < 4):
+    if(len(lvalues[len(lvalues) -1]) > 3):
+        hoursum = lvalues[len(lvalues) -1][3]
+    else:
+        hoursum = ''
+    while(len(lvalues) < 5):
         lvalues.append(['','','',''])
     lvalues_list_len = len(lvalues)
     for counter in range(0, lvalues_list_len):
         if counter + 1  == lvalues_list_len:
-            if(lvalues[counter][1] == ''):
+            if(lvalues[counter][1] == '' or lvalues[counter][2] == ''):
                 description = description + '&' + lvalues[counter][1] +' &' + lvalues[counter][2] + '\n\\\\\\tabucline[2pt]{1}\n\\tabulinestyle{2pt}\n\\multicolumn2{|r}{Summe: } \\vline & ' + hoursum + ' h \\\\\\tabucline[2pt]{1}'
             else:
                 description = description + '&' + lvalues[counter][1] +' &' + lvalues[counter][2] + ' h\n\\\\\\tabucline[2pt]{1}\n\\tabulinestyle{2pt}\n\\multicolumn2{|r}{Summe: } \\vline & ' + hoursum + ' h \\\\\\tabucline[2pt]{1}'
         else:
-            if(lvalues[counter][1] == ''):
-                description = description + '&' + lvalues[counter][1] +' &' + lvalues[counter][2] + '}\\\\\cline{2-3}\n'
+            if(lvalues[counter][1] == '' or lvalues[counter][2] == ''):
+                description = description + '&' + lvalues[counter][1] +' &' + lvalues[counter][2] + '\\\\\cline{2-3}\n'
             else:
-                description = description + '&' + lvalues[counter][1] +' &' + lvalues[counter][2] + ' h\\\\\cline{2-3}\n'
+                description = description + ' &' + lvalues[counter][1] +' &' + lvalues[counter][2] + ' h\\\\\cline{2-3}\n'
     return description
-
+newfilename = ''
 counter = 0
 valueslist = None
 newfile = None
@@ -54,9 +61,20 @@ for values in csvlist:
         valueslist = []
         with open("Vorlage.tex", "rt") as fin:
             for line in fin:
-                newfile.write(line.replace('%%WOCHE%%', date))
+                newfile.write(line.replace('%%WOCHE%%', date).replace('%%NAME%%', name).replace('%%DIVIS%%', abteilung).replace('%%YEAR%%', str(ausbildungsjahr)))
         newfile.seek(0)
     elif (values[0] == '' and values[1] != ''):
+        values[1] = str(values[1]).replace('&', '\&')
         print(values)
+        tvalues = []
         valueslist.append(values)
+if(valueslist is not None and valueslist != []):
+    descriptiontex = descriptionlinebuild(valueslist)
+    readfile = open(newfilename, 'rt')
+    for line in readfile:
+        newfile.write(line.replace('%%DESC%%', descriptiontex))
+    newfile.close()
+    proc=subprocess.Popen(shlex.split('pdflatex ' + newfilename))
+    proc.communicate()
+
 
